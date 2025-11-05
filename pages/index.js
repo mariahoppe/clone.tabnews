de748edd239c887f789a104bbee57d3847560f69
maria hoppe <maria.hoppe@unochapeco.edu.br>
2025-11-05 19:15:08 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,13 @@
 function Home() {
-    return <h1> Pai, hoje é um dia especial!
-        Feliz dia dos pais para o meu herói e meu fã número 1, sei o quanto me apoia e o quanto luta por mim todos os dias. 
-        Mais um ano se passou e mais um dia dos pais chegou, nesse ano nada mudou, meu amor, orgulho e agradecimaneto por você apenas crescem mais e mais, sei o quanto é difícil
-        não me ter por perto todos os dias, pra mim também não é fácil não ter vocês ao meu lado, mas sinto vocês sempre comigo, lembro de vocês a cada coisa que faço todos os dias que passam.
-        Esse ano não fiz carta física, mas fiz algo novo que aprendi recentemente, criei meu próprio espaço dentro da internet! Dentro desse meu primeiro espaço completamente online e que qualquer um do mundo pode acessar
-        decidi te homenagear pelo homem que és, um homem que sustenta, que ama, que apoia, que aconselha e que abraça, um pai, um marido, um filho um irmão... todos esse papéis você 
-        desempenha com muita excelência, não é novidade que você é meu exemplo de profissional e de pessoa - claro que a mãe também é -, você me ensinou a ser humilde, a ser empática com as pessoas
-        e que não precisamos tratá-las do mesmo jeito que nos tratam, o nosso caráter é a única coisa que importa. 
-        Te amo muito e vou sempre te amar, vou sempre correr pra você quando o mundo me maltratar - quando eu não conseguir me virar sozinha, é claro -, obrigada por ser esse pai incível,
-        todos cometemos erros, o importante é aprender com eles e trazer para as pessoas que machucamos o amor que elas merecem, outra lição que me ensinou e que levo comigo sempre!
-        Obrigada pai, te amo pra sempre.  </h1>
+  return <h1> oi </h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("identação errada");
 }
 
 export default Home;
